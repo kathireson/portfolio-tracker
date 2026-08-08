e9bde14f86d53379b85b7f3f50da5687ad84f87f
kathireson <kathireson+github@gmail.com>
2026-08-08 01:59:13 -0700

    Fixing Startup errors when using keycloak

--- src/main/java/com/homelab/portfolio/PortfolioTrackerApplication.java
+++ src/main/java/com/homelab/portfolio/PortfolioTrackerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = { OAuth2ClientAutoConfiguration.class })
+@SpringBootApplication
 @EnableScheduling
 public class PortfolioTrackerApplication {
 
